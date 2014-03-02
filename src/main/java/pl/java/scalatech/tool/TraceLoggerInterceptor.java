aa0b04fe1173b339b66609d0b190b64a04654adc
Slawomir Borowiec <przodownik@tlen.pl>
2014-03-02 15:56:04 +0100

    add some config files

---
+++ src/main/java/pl/java/scalatech/tool/TraceLoggerInterceptor.java
@@ -0,0 +1,11 @@
+package pl.java.scalatech.tool;
+
+/**
+ * @author Sławomir Borowiec 
+ * Module name : commonUtil
+ * Creating time :  2 mar 2014 15:52:06
+ 
+ */
+public class TraceLoggerInterceptor {
+
+}
